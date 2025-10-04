4bc3b58310eccf9c0eaad0b3e87e5f029f01f300
kumar485 <kumar485hyd@gmail.com>
2025-10-04 21:49:19 +0530

    immutable

---
+++ immutability.py
@@ -0,0 +1,8 @@
+# a=10
+# print(id(a))
+# a=a+10
+# print(id(a))
+
+a=10
+b=a
+print(a is b)
---
+++ typecasting.py
@@ -0,0 +1,78 @@
+#int
+#complex
+#str
+#float
+#bool
+
+
+# to convert from other types to int types
+# s=12.344 # 344 will be  gone
+# print(int(s))
+
+
+# print(int(10+6j))
+
+# print(int(True))
+# print(int(False))
+
+#str to convert to int
+
+#int to float 
+# print(float(23))
+# print(float(0xFace))
+
+#complex to float error
+
+
+#bool to float 
+# print(float(True))
+# print(float(False))
+
+
+#str to float
+
+# print(float('24'))
+# print(float('444.4'))
+
+
+
+
+#int ,float , bool ,str to   complex
+
+#complex(x)     complex(x,y)
+
+
+#complex(0)
+#complex(10.5)
+#complex(True)
+#complex(False)
+#complex('4')
+
+
+
+#bool
+# print(bool(0+0j))
+# #int,float,str,complex to boolen
+# #if arugment is 0 then false ,else True
+# print(bool(10))  
+# print(bool(0.0000001))
+
+# print(bool('True'))
+# print(bool('False'))
+
+# in string case only empty string is consider as false
+
+
+# if '':
+#     print("True")
+# else:
+#     print("False")
+
+
+
+
+
+# for string no loop holes we can convert andy thing
+
+
+print(str(0b1111))
