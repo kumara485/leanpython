03aa6b3a19341884d72816483d658fa9e002d404
kumar485 <kumar485hyd@gmail.com>
2025-10-04 18:41:12 +0530

    datatypestring

---
+++ datatypeb.py
@@ -0,0 +1,7 @@
+#Only allowed value True and False
+
+#True -1
+#False = 0
+print(True + True)
+print(True - False)
+
---
+++ datatypes.py
@@ -0,0 +1,63 @@
+s="durga"
+s='durga'
+print(s)
+j='a'
+print(type(j)) #no char data type
+tri="""kumar"""
+tiri='''kiran'''
+
+d="""
+kumr 
+kirna
+kkk
+"""
+print(d)
+
+# these are multiline string
+
+
+
+#to define multiline stting
+#to define ' and " 
+# to define doc string 
+
+#singe quote 
+#double quote
+
+st="kumar"
+print(st[0])
+print(st[1])
+# print(st[1000])
+
+
+
+#index 
+#str supports positive and negative index 
+
+# l[2:10:1] slice operators
+
+name="kumarghimre"
+# sl=name[4:10]
+# print(sl)
+# #index error is not raised by slice operator.
+# output=name[0].upper()+ name[1:]
+# print(output)
+# o=name[0].upper()+name[1:len(name)-1]+name[-1].upper()
+# print(o)
+# out=name[0].upper()+ name [:]
+
+
+#plus operator for stirng
+
+s='durga' +'  '+ 'soft'
+print(s)
+# ss='durga' + 10   both should be string , unlike java   
+# print(ss)
+
+
+# * operator  for string  ( one argument must be int and other stirng)
+#string repetation operator
+s= 10 * 'Kumar '
+print(s)
+
+#int,complex,str,bool,float   this is fundamental data types
