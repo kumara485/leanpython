c276dcf5f72462d44e1a8f354ee929c110e64acc
kumar485 <kumar485hyd@gmail.com>
2025-10-04 11:17:21 +0530

    reserverword

---
+++ reservewords.py
@@ -0,0 +1,16 @@
+#32 # Two are added later async and await
+#True,False,None
+#and,or ,not ,is 
+#if ,elif, else
+#while,for,break,continue,retun,in,yield
+#try,except,finally,raise,assert
+#import,from,as ,class,def,passs
+#global,nonlocal,lambda,del,with
+
+#True ,False, None are capital
+# No Switch,No Do while,etc
+
+import keyword
+
+print(len(keyword.kwlist))
+
