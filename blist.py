7d0a356f09604b211609ac4b0d40e4efbaedd45c
kumar485 <kumar485hyd@gmail.com>
2025-10-05 10:14:58 +0530

    tuplelist

---
+++ blist.py
@@ -0,0 +1,16 @@
+#list is mutable
+#insertion order is preserve
+# heterogenouse object are allowed.
+# indexing is present
+# slicing
+# []
+# append
+# remove
+# copy
+# reverse
+# sort
+# pop
+# clear
+# extend
+# count
+# index
---
+++ btuple.py
@@ -0,0 +1,9 @@
+# t=(1,2,3,4,5)
+# print(t[0])
+# print(t[-1])
+# print(t[1:4])
+#count
+#index
+
+t=(12,)
+print(type(t))
